76d79aa5ebee015653aac88f28f15552af0e4fa1
Matthew Chase Whittemore <mcwhittemore@gmail.com>
2020-04-30 22:43:43 -0400

    whitespace is dumb

--- img-scale.go
+++ img-scale.go
@@ -4,8 +4,8 @@ import (
   "fmt"
  "image"
  "image/jpeg"
-	"image/png"
-	"log"
+ "image/png"
+ "log"
  "os"
 )
 
@@ -25,14 +25,14 @@ func main() {
 
 func ScaleImage(filename string) {
   reader, err := os.Open(filename)
-	 if err != nil {
+  if err != nil {
     log.Fatal(err)
-	 }
+  }
 
-	 img, format, err := image.Decode(reader)
-	 if err != nil {
+  img, format, err := image.Decode(reader)
+  if err != nil {
     log.Fatal(err)
-	 }
+  }
 
   fitDim := 640
   yScale := img.Bounds().Max.Y / fitDim
@@ -48,36 +48,36 @@ func ScaleImage(filename string) {
 
   output := image.NewNRGBA(image.Rect(0,0,xMax,yMax))
 
-	 for y := 0; y < yMax; y++ {
-		  for x := 0; x < xMax; x++ {
+  for y := 0; y < yMax; y++ {
+    for x := 0; x < xMax; x++ {
       xPos := x * scale
       yPos := y * scale
       c := img.At(xPos, yPos)
       output.Set(x, y, c)
-		  }
-	 }
+    }
+  }
 
   f, err := os.Create(filename)
-	 if err != nil {
+  if err != nil {
     log.Fatal(err)
-	 }
+  }
 
   if format == "png" {
     if err := png.Encode(f, output); err != nil {
       f.Close()
-		    log.Fatal(err)
+      log.Fatal(err)
     }
   }
 
   if format == "jpeg" {
     if err := jpeg.Encode(f, output, &jpeg.Options{100}); err != nil {
       f.Close()
-		    log.Fatal(err)
+      log.Fatal(err)
     }
   }
 
 
-	 if err := f.Close(); err != nil {
+  if err := f.Close(); err != nil {
     log.Fatal(err)
-	 }
+  }
 }
